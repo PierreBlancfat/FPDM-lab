1b484e7ddc0ae3fcd0eb3cfc3a04861cd13ca274
polo <polo@debian.lan>
2019-11-06 11:53:02 +0100

    read and plot Amerge.txt

---
+++ lab1/lab.py
@@ -0,0 +1,47 @@
+import numpy as np
+import re
+import os
+from os import listdir
+from os.path import isfile, join
+from matplotlib import pyplot as plt
+
+def read_data(letter : str):
+
+    path = './Unistroke/Amerge.txt'
+    with open(path, 'r') as f:
+        nb_line = sum(1 for _ in f)
+    with open(path, 'r') as f:
+        data = np.zeros((nb_line,2))
+        i = 0
+        for line in f:
+            data[i][0] = float(line.strip().split(" ")[0])
+            data[i][1] = float(line.strip().split(" ")[1])
+            i+=1
+        return data
+
+
+mean1 = [-3,0]
+cov1 = [[5,-2],[-2,1]]
+
+mean2 = [3,0]
+cov2 = [[5,2],[2,2]]
+
+d1 = np.random.multivariate_normal(mean1, cov1, 500)
+d2 = np.random.multivariate_normal(mean2, cov2, 500)
+
+mixt = 0.3*d1 + 0.7*d2
+plt.step(d1[:,0], d1[:,1],".")
+plt.step(d2[:,0], d2[:,1],".")
+# plt.show()
+plt.step(mixt[:,0], mixt[:,1],".")
+
+
+plt.clf()
+data = read_data("A")
+print(data)
+plt.plot(data[:,0], data[:,1],'.')
+plt.show()
+
+            
+
+
